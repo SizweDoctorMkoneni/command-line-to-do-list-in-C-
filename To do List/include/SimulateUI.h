dc203b169258b0f46206b3930b939a925031cbbf
SizweDoctorMkoneni <sizwemkoneni@gmail.com>
2024-06-13 22:29:19 +0200

    implementation of the class that simulates the to do list

---
+++ To do List/include/SimulateUI.h
@@ -0,0 +1,15 @@
+#ifndef SIMULATEUI_H
+#define SIMULATEUI_H
+#include <fstream>
+#include <iomanip>
+#include "../TaskManager.h"
+
+
+class SimulateUI
+{
+public:
+    void show_menu(char& _key);
+    void simulateToDoList();
+};
+
+#endif // SIMULATEUI_H
--- To do List/main.cpp
+++ To do List/main.cpp
@@ -1,83 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <iomanip>
-#include "TaskManager.h"
+#include "SimulateUI.h"
 
 using namespace std;
 
-void show_menu(char& _key);
-
 int main()
 {
-    TaskManager myToDoList;
-    myToDoList.get_from_file();
-    ofstream listFile;
-    string myTask;
-    char key, priority;
-    show_menu(key);
 
-    while(toupper(key) != 'C')
-    {
-        if(toupper(key) == 'C'){
-            cout << "Closing to do list . . ." << endl;
-        }
-        else if(toupper(key) == 'A'){
-            cout << "Task: ";
-            cin.ignore();
-            getline(cin, myTask);
-            cout << "Priority (High - H, Intermmediate - I, Low - L): ";
-            cin >> priority;
-            myToDoList.add_task(myTask, priority);
-            show_menu(key);
-            cout << endl;
-        }
-        else if(toupper(key) == 'V'){
-            myToDoList.view_task_list();
-            show_menu(key);
-            cout << endl;
-        }
-        else if(toupper(key) == 'D'){
-            cout << "\nWhich task do you wish to delete?" << endl;
-            cout << "Task: ";
-            cin.ignore();
-            getline(cin, myTask);
-            myToDoList.delete_task(myTask);
-            show_menu(key);
-            cout << endl;
-        }
-        else if(toupper(key) == 'E'){
-            myToDoList.clear_task_list();
-            show_menu(key);
-            cout << endl;
-        }
-        else if(toupper(key) == 'P'){
-            cout << "\nWhich task do you wish to prioritise?" << endl;
-            cout << "Task: ";
-            cin.ignore();
-            getline(cin, myTask);
-            cout << "Priority (High - H, Intermmediate - I, Low - L): ";
-            cin >> priority;
-            myToDoList.change_task_status(myTask, priority);
-            show_menu(key);
-            cout << endl;
-        }
-    }
+    SimulateUI myToDoList;
 
+    myToDoList.simulateToDoList();
     return 0;
-}
-
-void show_menu(char& _key){
-    cout << " =============== TO DO LIST =============== \n\n";
-    cout << " --------------- Navigation keys ---------- \n\n";
-    cout << left << setw(14) << "Description" << right << setw(14) << "Key" << endl;
-    cout << left << setw(14) << "Add a task" << right << setw(14) << "A" << endl;
-    cout << left << setw(14) << "View task list" << right << setw(14) << "V" << endl;
-    cout << left << setw(14) << "Close to do list" << right << setw(12) << "C" << endl;
-    cout << left << setw(14) << "Delete task" << right << setw(14) << "D" << endl;
-    cout << left << setw(14) << "Clear task list" << right << setw(13) << "E" << endl;
-    cout << left << setw(14) << "Change task priority" << right << setw(8) << "P" << endl;
-    cout << endl;
-    cout << "Select key: ";
-    cin >> _key;
 }
 
